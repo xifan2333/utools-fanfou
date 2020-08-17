2faa6a0c1adcb91330fb13bfec8fd3632b321217
稀饭 <xifan233@163.com>
2020-08-18 04:43:28 +0800

    🎉 增添图标

--- src/main.js
+++ src/main.js
@@ -4,12 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import message from './components/message'
 Vue.use(message);
-// import VuetifyMessage from 'vuetify-message';
-// Vue.use(VuetifyMessage,{
-//   timeout: 1000,
-//   property: '$message',
-//   value:true
-// })
+
 Vue.config.productionTip = false
 Vue.prototype.$user = window.user
 Vue.prototype.$utools = window.utools
